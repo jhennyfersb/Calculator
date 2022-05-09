4c368b549d99fcac8fba9c7cecb6e9bc76296cb0
Jhennyfer Sobrinho <jhennyffersobrinho@gmail.com>
2022-05-09 11:45:10 -0300

    Calculadora simples de pagamento de empréstimo

--- src/com/jhenny/Calculator.java
+++ src/com/jhenny/Calculator.java
@@ -1,36 +1,50 @@
 package com.jhenny;
 
 public class Calculator {
-    //O programa calcula dois numeros inteiros
-    public int add(int a, int b) {
-        return a + b;
+    //Classe de exemplo do exercico para Calcular os valores
+
+    public static void soma(double num1, double num2){
+        double resultado = num1 + num2;
+        System.out.println("a soma de " + num1 + " mais " + num2 + " é " + resultado);
     }
 
-    public int subtract(int a, int b) {
-        return a - b;
+    public static void subtracao(double num1 , double num2){
+        double resultado = num1 - num2;
+        System.out.println("a subtração de " + num1 + " menos " + num2 + " é " + resultado);
     }
-
-    public int multiply(int a, int b) {
-        return a * b;
+    public static void divisao( double num1, double num2){
+        double resultado = num1 / num2;
+        System.out.println("a divisão de " + num1 + " dividido " + num2 + " é " + resultado);
     }
-
-    public int divide(int a, int b) {
-        return a / b;
-    }
-
-    public int modulo(int a, int b) {
-        return a % b;
-    }
-
-    public static void main(String[] args) {
-        Calculator myCalculator = new Calculator();
-        System.out.println(myCalculator.add(5, 7));
-        System.out.println(myCalculator.subtract(45, 11));
-        System.out.println(myCalculator.multiply(55, 12));
-        System.out.println(myCalculator.divide(5, 6));
-        System.out.println(myCalculator.modulo(5, 6));
-
+    public static void multiplicacao( double num1, double num2){
+        double resultado = num1 * num2;
+        System.out.println("a multplicação de " + num1 + " vezes " + num2 + " é " + resultado);
 
     }
+    public static void main(String[] args){
 
+        //Calculadora
+        System.out.println("exercicio calculadora");
+        Calculator.soma(3,6);
+        Calculator.soma(9,1.8);
+        Calculator.divisao(5,2.5);
+        Calculator.multiplicacao(7,8);
+        //Mensagem
+        System.out.println("Exercicio mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+
+        //Empréstimo
+        System.out.println("Exercicio empréstimo");
+        Emprestimo.calcular(1000,Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000,Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000,5);
+
+
+
+
+
+
+    }
 }
---
+++ src/com/jhenny/Emprestimo.java
@@ -0,0 +1,29 @@
+package com.jhenny;
+
+public class Emprestimo {
+    //Classe de exemplo para taxa do empréstimo
+    public static int getDuasParcelas(){
+        return 2;
+    }
+    public static int getTresParcelas(){
+        return 3;
+    }
+    public static double getTaxaDuasParcelas(){
+        return 0.3;
+    }
+    public static double gettaxaTresParcelas(){
+        return 0.45;
+    }
+    public static void calcular(double valor, int parcelas){
+        if (parcelas == 2){
+            double valorFinal = valor + (valor * getTaxaDuasParcelas());
+            System.out.println("Valor final do empréstimo para 2 parcelas: R$ " + valorFinal);
+        }else if(parcelas == 3){
+            double valorFinal = valor + (valor * gettaxaTresParcelas());
+            System.out.println("Valor final do empréstimo para 3 parcelas: R$ " + valorFinal);
+        }else{
+            System.out.println("Quantidade de parcelas não aceita");
+        }
+    }
+
+}
